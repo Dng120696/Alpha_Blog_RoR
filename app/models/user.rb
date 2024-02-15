566d39415618823421a0b61cadb7e44b9a9906a3
Christian Patrick Nebab <dongyang0216@gmail.com>
2024-02-15 18:17:35 +0800

    add user validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ApplicationRecord
-
+    VALID_EMAIL_REGEX = /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
+    validates :username, presence: true,
+                        uniqueness: { case_sensitive: false },
+                        length: {minimum:3,maximum:25}
+    validates :email, presence: true,
+                        uniqueness: { case_sensitive: false },
+                        length: {maximum:105},
+                        format: { with: VALID_EMAIL_REGEX, message: "Invalid email format" }
 end
