196b88de62687c2f9f567ce54527138c31d58a4a
Christian Patrick Nebab <dongyang0216@gmail.com>
2024-02-21 14:23:14 +0800

    add one to many association between users and articles from the backend

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+    has_many :articles
     VALID_EMAIL_REGEX = /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
     validates :username, presence: true,
                         uniqueness: { case_sensitive: false },
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_15_090920) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_15_104726) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "description"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
